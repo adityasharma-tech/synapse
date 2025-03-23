459e9a13e905df0a6b9e2420bd96097648ef51ab
Aditya Sharma <aadisharma.in+github@gmail.com>
2025-03-23 13:12:11 +0530

    Enhance login functionality and implement user redirection in socket layout

--- web/src/app/stream/_layout.tsx
+++ web/src/app/stream/_layout.tsx
@@ -1,7 +1,21 @@
-import { Outlet } from "react-router";
+import { Outlet, useNavigate } from "react-router";
 import { SocketProvider } from "../../hooks/socket.hook";
+import { useAppSelector } from "../../store";
+import React from "react";
 
 export default function SocketLayout() {
+  const user = useAppSelector((state) => state.app.user);
+  const navigate = useNavigate();
+
+  React.useEffect(() => {
+    if (!user) {
+      const searchParams = new URLSearchParams()
+      searchParams.set('redirect_uri', window.location.pathname)
+      navigate(`/auth/login?${searchParams.toString()}`);
+    }
+  }, [user]);
+
+  if (!user) return;
 
   return (
     <SocketProvider>
