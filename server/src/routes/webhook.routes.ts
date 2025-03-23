2bfb3a93f413ad162b70bd9bf57949cc573770d5
Aditya Sharma <aadisharma.in+github@gmail.com>
2025-03-23 19:05:41 +0530

    Deployment webhook fix: Added a middlware on webhook route to override default cors policy set using the global package middeware

--- server/src/routes/webhook.routes.ts
+++ server/src/routes/webhook.routes.ts
@@ -3,6 +3,13 @@ import { handleVerifyCfOrder } from "../controller/webhook.controller";
 
 const router = Router();
 
+router.use((_, res, next)=>{
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Methods", "POST");
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+    next()
+})
+
 router.route("/cf").post(handleVerifyCfOrder);
 
 export default router;
