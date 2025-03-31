d4bb615de31b66630b61f79d123310af938d26f4
Aditya Sharma <aadisharma.in+github@gmail.com>
2025-03-31 18:22:08 +0530

    Add Razorpay integration to payout worker; update environment configuration, install Razorpay SDK, and implement order fetching and transfer creation logic

--- queueWorker/src/workers/payout.worker.ts
+++ queueWorker/src/workers/payout.worker.ts
@@ -1,23 +1,49 @@
-import type { RabbitMq } from "../rmq"
+import type { RabbitMq } from "../rmq";
 import type { ConsumeMessage } from "amqplib";
 import { PAYOUT_WORKER_QUEUE, DEFAULT_CHANNEL } from "../lib/constants";
+import { getRazorpayInstance } from "../lib/utils";
 
-async function payoutWorker(rmq: RabbitMq){
-    if(!rmq.channel) throw new Error("Rabbit MQ not connected yet!");
-    
-    await rmq.loadChannel(DEFAULT_CHANNEL, {
-        durable: true
-    })
+const razorpayInstance = getRazorpayInstance();
 
-    console.log(`Payout worker is running...`);
+async function payoutWorker(rmq: RabbitMq) {
+  if (!rmq.channel) throw new Error("Rabbit MQ not connected yet!");
 
-    rmq.channel.consume(PAYOUT_WORKER_QUEUE, payoutWorkerHandler)
+  await rmq.loadChannel(DEFAULT_CHANNEL, {
+    durable: true,
+  });
+
+  console.log(`Payout worker is running...`);
+
+  rmq.channel.consume(PAYOUT_WORKER_QUEUE, payoutWorkerHandler);
 }
 
-function payoutWorkerHandler(msg: ConsumeMessage | null){
-    if(!msg) return;
+async function payoutWorkerHandler(msg: ConsumeMessage | null) {
+  if (!msg) return;
 
-    
+  console.log(`Payload worker message: ${JSON.stringify(msg)}`);
+
+  const payload = JSON.parse(msg.content.toString()) as {
+    orderId: string;
+  };
+
+  const orderId = payload.orderId;
+
+  if (!orderId) return console.error(`Failed to get OrderID: ${orderId}`);
+
+  try {
+    const order = await razorpayInstance.orders.fetch(orderId);
+
+    await razorpayInstance.transfers.create({
+      account: "1",
+      amount: 1,
+      currency: "INR",
+      notes: {
+        orderId: order.id
+      },
+    });
+  } catch (error: any) {
+    console.error(`Failed to get message: ${error.message}`);
+  }
 }
 
-export default payoutWorker
+export default payoutWorker;
