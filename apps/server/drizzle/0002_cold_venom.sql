7d26201f674446bc562fa2a7bc25e6027f8ded7d
Aditya Sharma <aadisharma.in+github@gmail.com>
2025-04-24 23:12:23 +0530

    Created dockerfile only --no-config, husky pre-commit update format with prettier

--- apps/server/drizzle/0002_cold_venom.sql
+++
@@ -1,4 +0,0 @@
-CREATE TYPE "upgrade"."last_login_method" AS ENUM('email-password', 'sso/google', 'sso/github');--> statement-breakpoint
-ALTER TABLE "upgrade"."token_table" ALTER COLUMN "reset_password_token_expiry" SET DEFAULT '2025-04-08 07:48:59.896';--> statement-breakpoint
-ALTER TABLE "upgrade"."token_table" ALTER COLUMN "email_verification_token_expiry" SET DEFAULT '2025-04-08 07:48:59.896';--> statement-breakpoint
-ALTER TABLE "upgrade"."users" ADD COLUMN "last_login_method" "upgrade"."last_login_method" DEFAULT 'email-password';
