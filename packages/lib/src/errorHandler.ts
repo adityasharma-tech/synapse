26a0750d6996f5fc0e3264f25ef631fc4abd3bb1
Aditya Sharma <aadisharma.in+github@gmail.com>
2025-04-28 16:36:43 +0530

    fix: errorHandle error while json parsing fixed and increased lerna concurrency

--- packages/lib/src/errorHandler.ts
+++ packages/lib/src/errorHandler.ts
@@ -1,13 +1,22 @@
 import { logger } from "./logger";
-import { Request, Response } from "express";
+import { NextFunction, Request, Response } from "express";
 import { ApiError, ErrCodes } from "./ApiError";
 
-type ErrorHandler = (err: any, _: Request, res: Response) => void;
+type ErrorHandler = (
+    err: any,
+    _: Request,
+    res: Response,
+    __: NextFunction
+) => void;
 
-const errorHandler: ErrorHandler = (err, _, res) => {
+const errorHandler: ErrorHandler = (err, _, res, __) => {
+    try {
+        logger.error(`Server: ${JSON.stringify(err)}`);
+    } catch (error) {
+        console.error(err);
+    }
     if (err instanceof ApiError)
         return res.status(err.statusCode ?? 500).json(err);
-    logger.error(`Server: ${JSON.stringify(err)}`);
     res.status(500).json(
         new ApiError(500, "Internal server error", ErrCodes.DEFAULT_RES)
     );
